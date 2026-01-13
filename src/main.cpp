0b3a549fe2ef60ceab6ebbcc6870319c5767653d
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2026-01-12 17:13:45 -0800

    Initial FreeRTOS tasks proposal

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,118 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// https://pvs-studio.com
+
+/* Includes */
+#include "2026Core/Net/Net-Link/AdapterUHCI.hpp"
+// #include "2026Core/Net/NetAdapter_A.hpp"
+#include "esp_log.h"
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/* Config */
+static constexpr char *TAG = "NaMa";
 
+/* Function Prototypes */
+
+/**
+ * MARK: Setup
+ * put your setup code here, to run once:
+ */
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(LED::LED_PIN, OUTPUT);
+
+    // Set up tasks
+    xTaskCreate(vTaskStatusLED, // Task function
+                "Status LED",   // Name of the task (for debugging)
+                1024,           // Stack size (in words, not bytes)
+                nullptr,        // Task input parameter
+                1,              // Priority of the task
+                nullptr         // Task handle
+    );
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/**
+ * MARK: Tasks
+ * @see
+ * https://www.freertos.org/Documentation/02-Kernel/02-Kernel-features/01-Tasks-and-co-routines/05-Implementing-a-task
+ * @see https://forum.arduino.cc/t/non-blocking-delay-actions/1044079
+ */
+
+void vTaskStatusLED(void *pvParameters) {
+    while (true) {
+        digitalWrite(LED::LED_PIN, HIGH);
+        delay(LED::BLINK_ON_MILLIS);
+        digitalWrite(LED::LED_PIN, LOW);
+        delay(LED::BLINK_OFF_MILLIS);
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/**
+ * @brief Task to handle Telnet connections
+ */
+void vTaskTelnet(void *pvParameters) {
+    while (true) {
+    }
 }
+
+/**
+ * @brief Task to handle ElegantOTA connections
+ * @deprecated Just use a USB cable if possible
+ */
+void vTaskOTA(void *pvParameters) {
+    while (true) {
+    }
+}
+
+/**
+ * @brief Task to poll high priority sensors
+ */
+void vTaskPollSensors(void *pvParameters) {
+    while (true) {
+    }
+}
+
+/**
+ * @brief Task to control the pitch actuator
+ */
+void vTaskPitch(void *pvParameters) {
+    while (true) {
+    }
+}
+
+/**
+ * @brief Task to handle inbound data that has been queued
+ */
+void vTaskHandleInboundData(void *pvParameters) {
+    while (true) {
+    }
+}
+
+/**
+ * @brief Task to handle outbound data that has been queued
+ */
+void vTaskHandleOutboundData(void *pvParameters) {
+    while (true) {
+    }
+}
+
+/**
+ * @brief Task to log data
+ */
+void vTaskLogData(void *pvParameters) {
+    while (true) {
+    }
+}
+
+/**
+ * @brief Task to track idle time
+ */
+void vTaskIdle(void *pvParameters) {
+    while (true) {
+    }
+}
+
+/**
+ * MARK: loop
+ * Arduino: put your main code here, to run repeatedly:
+ */
+void loop() {}
