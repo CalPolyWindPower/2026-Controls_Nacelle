78a638c8752bff198f1f18fcaa3e32377482b331
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2026-03-17 02:50:10 -0700

    ActuatorDemo: Improved spelling, formatting, and added impl. docs.

--- demos/ActuatorDemo/ActuatorDemo.ino
+++ demos/ActuatorDemo/ActuatorDemo.ino
@@ -1,19 +1,20 @@
 /**
  * @file ActuatorDemo.ino
  * @brief Manually control the actuator
- * @version 1.0.0
+ * @version 1.1.0
  * @since Winter 2026
- * @author Noah (@BobSaidHi <https://github.com/bobsaidhi>) for @CalPolyWindPower <https://github.com/calpolywindpower>
- * @author Inspired by Trevor (@rover-t <https://github.com/rover-t>) at @CalPolyWindPower <https://github.com/calpolywindpower>
+ * @author Noah (@BobSaidHi <https://github.com/bobsaidhi>) for
+ * @CalPolyWindPower <https://github.com/calpolywindpower>
+ * @author Inspired by Trevor (@rover-t <https://github.com/rover-t>) at
+ * @CalPolyWindPower <https://github.com/calpolywindpower>
  *
- * The DFRobot Firebeetle 2 ESP32-C5 has one USB C port.  The baud
- *   rate is seto to 115,000 below and uses the internal USB
- *   perheprial, not a seperate chip.
- * Some compatible serial monitors include the Arduino IDE
- *   <https://www.arduino.cc/en/software/>, VSCode "Serial" extension
- *   from Microsoft
- *   <https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-serial-monitor>,
- *   or PuTTY.
+ * The DFRobot FireBeetle 2 ESP32-C5 has one USB C port.  The baud rate is set
+ * to to 115,000 below and uses the internal USB peripheral, not a separate
+ * chip. Some compatible serial monitors include the Arduino IDE
+ * <https://www.arduino.cc/en/software/>, VSCode "Serial" extension from
+ * Microsoft
+ * <https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-serial-monitor>,or
+ * PuTTY.
  *
  * To re-flash:
  * 1. Install Arduino IDE (I recommend >v2.0) from
@@ -24,8 +25,8 @@
  *      File > Preferences > Settings >
  *      Additional Board Manager URLS:
  *      `https://espressif.github.io/arduino-esp32/package_esp32_index.json`
- *     (See also:
- * https://docs.espressif.com/projects/arduino-esp32/en/latest/installing.html)
+ *      (See also:
+ *       https://docs.espressif.com/projects/arduino-esp32/en/latest/installing.html)
  * 3. Load the attached sketch or the latest version from GitHub:
  *    https://github.com/CalPolyWindPower/2026-Controls_Nacelle/blob/main/demos/ActuatorDemo/ActuatorDemo.ino
  * 4. Set the board to "ESP32C5 Dev Module".  There may be multiple
@@ -37,48 +38,82 @@
  * 8. Open a serial terminal
  *   A. In Arduino IDE, got Tools > Serial Monitor and switch to 115200 baud
  *
- * @author Noah (@BobSaidHi <https://github.com/bobsaidhi>) for
- *   Cal Poly Wind Power (@calpolywindpower
- * <https://github.com/calpolywindpower>)
+ * @author Noah (@BobSaidHi <https://github.com/bobsaidhi>) for Cal Poly Wind
+ * Power (@calpolywindpower <https://github.com/calpolywindpower>)
  * @see Inspired by
- *   https://github.com/rover-t/CPWP2024-Controls/blob/main/ControlCode/OpenLoopController/OpenLoopController.ino
+ * https://github.com/rover-t/CPWP2024-Controls/blob/main/ControlCode/OpenLoopController/OpenLoopController.ino
  * @see Inspired by
- *   https://github.com/rover-t/CPWP2024-Controls/blob/main/ControlCode/OpenLoopController_alt/OpenLoopController_alt.ino
+ * https://github.com/rover-t/CPWP2024-Controls/blob/main/ControlCode/OpenLoopController_alt/OpenLoopController_alt.ino
  */
 
-// Imports
+// MARK: Imports
 #include <Arduino.h>
 #include <cstdint> // Fixed size integers
-#if defined(ESP32)
 /**
+ * @details Only use ESP32 Servo on the ESP32, and use our custom one for better
+ * ESP32 C5 support.
  * @see https://github.com/madhephaestus/ESP32Servo/blob/master/src/ESP32Servo.h
+ * @see https://docs.arduino.cc/libraries/servo/
  */
+#if defined(ESP32)
 // #define ENFORCE_PINS 0
 // #include <ESP32Servo.h>
-#include "./src/ESP32Servo/src/ESP32PWM.h"
-#include "./src/ESP32Servo/src/ESP32Servo.h"
+#    include "./src/ESP32Servo/src/ESP32PWM.h"
+#    include "./src/ESP32Servo/src/ESP32Servo.h"
 #else
 #    include <Servo.h>
 #endif
 
-// Varialbes
+// MARK: Globals
+
+/**
+ * @brief Configuration for serial (UART) communication
+ * @details A namespace can be used to organize related constants, variables,
+ * and objects together.
+ * @see https://www.geeksforgeeks.org/cpp/namespace-in-c/
+ * @details `constexpr` is similar to `#define` macros but type checked.  Macros
+ * and  `constexpr` variables are guaranteed and to be evaluated at compile
+ * time, and may even be embedded in the immediate field of an instruction,
+ * which means they do not take up additional storage space, memory, or
+ * registers at turn time.
+ * @see https://en.cppreference.com/w/cpp/language/constexpr.html
+ * @see https://www.sciencedirect.com/topics/computer-science/immediate-operand
+ */
 namespace SERIAL_CONFIG {
-    constexpr long BAUD = 115200;
+    constexpr long BAUD = 115200; // BAUD rate (bits per second)
 } // namespace SERIAL_CONFIG
 
+/**
+ * @brief Configuration for the actuator and related pins
+ * @details The standard integer types, such as int, only specify a minimum
+ * size.  FEEDBACK_PIN is of the fixed width unsigned integer type `uint8_t`,
+ * which is always 8 bits.  This was chosen because the `pinmode()` function
+ * takes an `uint8_t` as an argument.
+ */
 namespace Actuator {
     Servo device = Servo();
     constexpr int MIN_POS_us = 1000;    // Minimum position in microseconds
     constexpr int MAX_POS_us = 2000;    // Maximum position in microseconds
     constexpr int DEFAULT_us = 1500;    // Default position in microseconds
     constexpr int CONTROL_PIN = 3;      // PWM pin
-    constexpr uint8_t FEEDBACK_PIN = 2; // Analgo feedback pin
+    constexpr uint8_t FEEDBACK_PIN = 2; // Analog feedback pin
 } // namespace Actuator
 
+/**
+ * @brief Configuration for the LED and related pins
+ * @details As we have more program storage space, and hopefully more RAM than
+ * we need, the slight performance boost from the fast integer types, such ast
+ * uint_fast32_t, are likely worth it for us.  These types specify a minium
+ * size, but allow the compiler (actually the system/ standard libraries) to
+ * substitute a type that a particular chip (micro-architecture) is more
+ * optimized for.
+ */
 namespace LED {
-    constexpr uint8_t PIN = 15;
-    constexpr uint_fast32_t TIME_ON_MS = 1000;
-    constexpr uint_fast32_t TIME_OFF_MS = 3000;
+    constexpr uint8_t PIN = 15; // LED pin
+    constexpr uint_fast32_t TIME_ON_MS =
+        1000; // Time LED stays on in milliseconds
+    constexpr uint_fast32_t TIME_OFF_MS =
+        3000; // Time LED stays off in milliseconds
 } // namespace LED
 
 /**
@@ -104,21 +139,24 @@ void setup() {
 
     // Servo device = Servo();
 
+    /**
+     * @details The Servo object, device, is in the Actuator namespace
+     */
     Actuator::device.attach(Actuator::CONTROL_PIN, Actuator::MIN_POS_us,
                             Actuator::MAX_POS_us); // Attach actuator
     Actuator::device.writeMicroseconds(Actuator::DEFAULT_us);
 
-    Serial.println("Initalized 2026 Actuator Demo v2026-3-5 / v1.0.0");
+    Serial.println("Initalized 2026 Actuator Demo v2026-3-5 / v1.1.0");
     digitalWrite(LED::PIN, LOW); // Toggle LED
 
-    Serial.print("Input servo position in Microseoncds: ");
+    Serial.print("Input servo position in Microseconds: ");
 }
 
 /**
  * @details put your main code here, to run repeatedly:
  */
 void loop() {
-    // Check if Serial data is avaiable
+    // Check if Serial data is available
     if (Serial.available() > 0) {
         int position = Serial.parseInt();
 
@@ -140,7 +178,7 @@ void loop() {
     }
 
 #if defined(ESP32)
-    // Toggle LED because why not
+    // Toggle LED because why not, but only on the EPS32
     static int lastLEDTime_ms = 0; // Track time toggled
     static bool LEDOn = false;     // Track LED state
 
