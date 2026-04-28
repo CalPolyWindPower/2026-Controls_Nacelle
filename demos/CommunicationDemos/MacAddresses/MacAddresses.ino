90b1cd7f76b61e7ec70d173f53d8762f33255f18
arloeitreim <105323402+arloeitreim@users.noreply.github.com>
2026-04-27 21:15:25 -0700

    Create MacAddresses
    upload to a controller to find the MAC Address of given controller.
    
    MacAddresses.txt contains the current mac addresses of the nacelle and load controllers.

---
+++ demos/CommunicationDemos/MacAddresses/MacAddresses.ino
@@ -0,0 +1,17 @@
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA);
+  delay(100);
+
+  Serial.println();
+  Serial.println("ESP32 MAC Address Finder");
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+}
